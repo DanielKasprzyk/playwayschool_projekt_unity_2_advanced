48a49bcae488e8b77a8b04bcaa8e5e48ebcb546c
Daniel Kasprzyk <dkwarmaster3@gmail.com>
2020-12-01 21:09:31 +0100

    project files

---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Patrol : MonoBehaviour
+{
+    public Transform[] waypoints;
+    public int startPoint = 0;
+    public float speed = 0.5f;
+    public float distanceTreshold = 0.5f;
+
+    private int selectedPoint;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        selectedPoint = startPoint;
+        agent = gameObject.GetComponent<NavMeshAgent>();
+
+        ChangeEnabledState();
+    }
+
+    void Update()
+    {
+        ChangeEnabledState();
+
+        GoToDestination();
+    }
+
+    public void GoToDestination()
+    {
+        if (agent.remainingDistance < distanceTreshold)
+        {
+            agent.destination = waypoints[selectedPoint].position;
+
+            selectedPoint = GetNextDestinationPoint(selectedPoint, waypoints.Length);
+        }
+    }
+    public int GetNextDestinationPoint(int point, int pointsCount)
+    {
+        return (point + 1) % pointsCount;
+    }
+
+    public void ChangeEnabledState()
+    {
+        if (waypoints.Length < 2)
+        {
+            enabled = false;
+        }
+        else
+        {
+            enabled = true;
+        }
+    }
+}
